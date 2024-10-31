6f2af65e9b30879ce3f78370e010c6254e908fa1
Pedro de Oliveira Santos <158167681+pedro-ols@users.noreply.github.com>
2024-10-31 07:52:49 -0300

    Initial commit

---
+++ src/routes/usuarios.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const usuariosRoutes = Router();
+
+let usuarios = [];
+
+usuariosRoutes.get("/", (req, res) =>{
+    return res.status(200).json({
+        message: usuarios.length == 0 
+        ? "Não há usuários cadastrados"
+        : `Total de usuários: ${usuarios.length}`, 
+    usuarios,
+    });
+});
+
+export default usuariosRoutes;
